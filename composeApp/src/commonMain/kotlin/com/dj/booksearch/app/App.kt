36c70982e0c9b38a4095bbaa0fb1165e5516fa45
DJDrama <putzzang@naver.com>
2024-11-29 17:23:47 +0900

    Animations

--- composeApp/src/commonMain/kotlin/com/dj/booksearch/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/dj/booksearch/app/App.kt
@@ -1,5 +1,7 @@
 package com.dj.booksearch.app
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -34,7 +36,14 @@ fun App() {
             navigation<Route.BookGraph>(
                 startDestination = Route.BookList
             ) {
-                composable<Route.BookList> {
+                composable<Route.BookList>(
+                    exitTransition = {
+                        slideOutHorizontally()
+                    },
+                    popEnterTransition = { // sliding in
+                        slideInHorizontally()
+                    }
+                ) {
                     val bookListViewModel = koinViewModel<BookListViewModel>()
                     val selectedBookViewModel =
                         it.sharedKoinViewModel<SelectedBookViewModel>(navController = navController)
@@ -52,7 +61,18 @@ fun App() {
                         }
                     )
                 }
-                composable<Route.BookDetail> {
+                composable<Route.BookDetail>(
+                    enterTransition = {
+                        slideInHorizontally { initialOffset ->
+                            initialOffset
+                        }
+                    },
+                    exitTransition = {
+                        slideOutHorizontally { initialOffset ->
+                            initialOffset
+                        }
+                    }
+                ) {
                     val selectedBookViewModel =
                         it.sharedKoinViewModel<SelectedBookViewModel>(navController = navController)
                     val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifecycle()
